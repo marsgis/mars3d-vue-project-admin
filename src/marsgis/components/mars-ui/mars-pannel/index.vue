a23af846cfe76f9adb440f14d229608b05e67330
wanghao <wh@marsgis.cn>
2023-03-17 09:55:13 +0800

    同步代码 v3.5.0

--- src/marsgis/components/mars-ui/mars-pannel/index.vue
+++ src/marsgis/components/mars-ui/mars-pannel/index.vue
@@ -16,7 +16,7 @@
 /**
  * pannel面板
  * @copyright 火星科技 mars3d.cn
- * @author 火星吴彦祖 2022-02-19
+ * @author 木遥 2022-02-19
  */
 import { onMounted, ref, computed } from "vue"
 import { getConfig } from "../index"
